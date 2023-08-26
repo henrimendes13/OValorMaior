e5c84f7cfb14907968902b50c8cfc9b0f4d45bf2
Henrique Mendes <henriquemendes13@gmail.com>
2023-08-26 00:17:26 -0300

    Add project files.

---
+++ OValorMaior/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido 
+ da mensagem “é o maior”.*/
+
+int a, b, c, maior;
+
+a=int.Parse(Console.ReadLine());
+b=int.Parse(Console.ReadLine());
+c=int.Parse(Console.ReadLine());
+
+maior = Math.Max(a,Math.Max(b,c));
+
+Console.WriteLine(maior+" É o maior.");
